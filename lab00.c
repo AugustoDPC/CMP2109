54e886fd8c480eb657eb2b5673a330b396bd9ba8
AugustoDPC <142420017+AugustoDPC@users.noreply.github.com>
2025-09-08 23:03:51 +0000

    tarefa

---
+++ lab00.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+#define MAX_COL 10
+
+int buscarLinear(int arr[], int n, int x){
+    for(int i= 0;i < n; i++){
+        if(int i = 0; i < n; i++){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int somaMatriz(int matriz[][MAX_COL], int m, int n){
+    int soma = 0;
+    for(int = 0;i < m;i++){
+        for(int j = 0; j< n;j++){
+            soma+= matriz[i][j];
+        }
+    }
+    return soma;
+}
+
+int fatorial(int n){
+    fi( n == 0){
+        return 1;
+    }else{
+        return n*fatorial(n-1)
+    }
+}
+
+void exemplo(int n){
+        for(int = 0;i < m;i++){
+        for(int j = 0; j< n;j++){
+            soma+= matriz[i][j];
+        }
+    }
+}
+
+
+int main(){
+    int vetor[] = {4,6,8,7,2};
+    int n = sizeof(v) ; sizeof(v[0]);
+    int alvo = 7;
+    int pos = buscaLinear{v, n, alvo};
+    
+}
